d967e4b0bf6e76e5ae749ab64892cbf6a29aa366
Niamh Nikali <niamh@elixxir.io>
2020-11-24 14:17:33 -0800

    Fix cpu build

--- reveal_cpu.go
+++ reveal_cpu.go
@@ -16,6 +16,6 @@ import (
 
 // RevealChunk is stubbed unless GPU is present.
 var RevealChunk RevealChunkPrototype = func(p *StreamPool, g *cyclic.Group,
-	publicCypherKey *cyclic.Int, cypher *cyclic.IntBuffer) error {
+	publicCypherKey *cyclic.Int, cypher *cyclic.IntBuffer, result *cyclic.IntBuffer) error {
 	return errors.New(NoGpuErrStr)
 }
