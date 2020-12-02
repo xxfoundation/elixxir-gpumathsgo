67173026f29aed963892dd231465503fdddd7ca1
Niamh Nikali <niamh@elixxir.io>
2020-12-02 10:47:14 -0800

    Add mul3chunk def for cpu

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gitlab.com/elixxir/gpumathsgo
 go 1.13
 
 require (
-	gitlab.com/elixxir/crypto v0.0.5-0.20201124195819-54be458a0dac
-	gitlab.com/xx_network/crypto v0.0.5-0.20201124194022-366c10b1bce0
+	github.com/pkg/errors v0.9.1
+	gitlab.com/elixxir/crypto v0.0.7-0.20201201231824-67328e9b6426
+	gitlab.com/xx_network/crypto v0.0.5-0.20201201003055-12e514a638ae
 )
---
+++ mul3_cpu.go
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
+//+build !linux !gpu
+
+package gpumaths
+
+import (
+	"github.com/pkg/errors"
+	"gitlab.com/elixxir/crypto/cyclic"
+)
+
+// Mul3Chunk is stubbed unless GPU is present.
+var Mul3Chunk Mul3ChunkPrototype = func(p *StreamPool, g *cyclic.Group,
+	x *cyclic.IntBuffer, y *cyclic.IntBuffer, z *cyclic.IntBuffer, result *cyclic.IntBuffer) error {
+	return errors.New(NoGpuErrStr)
+}
